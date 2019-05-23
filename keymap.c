0c856e7b2ee0bb38cef78c1335b9426119f39747
Guido Schmidt <git@guidoschmidt.cc>
2019-05-23 09:42:26 +0200

    EDIT/keymap.c: Adjust programming layer — add parentheses variations

--- keymap.c
+++ keymap.c
@@ -12,11 +12,15 @@ extern keymap_config_t keymap_config;
 #define _FNQ    1
 #define _DVORAK 2
 #define _FND    3
+#define _FNPP   4
 
 #define MO_FN MO(_FNQ)
 #define MO_FD MO(_FND)
+#define MO_PP MO(_FNPP)
 
 #define TG_DV TG(_DVORAK)
+
+#define DE_MORE LSFT(DE_LESS)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -36,22 +40,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______,   KC_UP, _______, _______, _______, _______, _______, \
     _______, DE_AE,   _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,\
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_DVORAK] = LAYOUT(
-    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    DE_PLUS, DE_CIRC, KC_DEL,  KC_BSPC, \
-    KC_TAB,  DE_UE,   KC_COMM,  KC_DOT, KC_P,    DE_Y,    KC_F,    KC_G,    KC_C,    KC_T,    DE_Z,    DE_SS,   DE_QST, KC_BSLS, \
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    DE_PLUS, DE_LESS, _______, KC_BSPC, \
+    KC_TAB,  DE_UE,   KC_COMM, KC_DOT,  KC_P,    DE_Y,    KC_F,    KC_G,    KC_C,    KC_T,    DE_Z,    DE_SS,   DE_QST,  KC_BSLS, \
     KC_LCTL, KC_A,    KC_O,    KC_E,    KC_I,    KC_U,    KC_H,    KC_D,    KC_R,    KC_N,    KC_S,    KC_L,    DE_MINS, \
     KC_LSFT, DE_OE,   KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    DE_HASH, KC_UP,   TG_DV, \
-    MO_FD,   KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,           KC_ENT,  _______, KC_RALT, KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT
+    MO_FD,   KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,           KC_ENT,  _______, KC_RALT, KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FND] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL, \
     _______, _______, _______, _______, _______, _______, _______, _______,   KC_UP, _______, _______, _______, _______, _______, \
-    _______, DE_AE,   _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,\
+    _______, DE_AE  ,_______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,\
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
+    _______, MO_PP  , _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
   ),
+
+  [_FNPP] = LAYOUT(
+    _______, _______, _______, _______, _______, _______, _______, DE_LESS, DE_MORE, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, DE_LBRC, DE_LCBR, DE_RCBR, DE_RBRC, _______, _______, _______, _______, \
+    _______, DE_AE  , _______, _______, _______, _______, _______, DE_LPRN, DE_RPRN, _______, _______, _______, _______,\
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, MO_PP  , _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
+  )
 };
